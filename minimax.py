10afbd00c4044e34f55bef92fcc22289d5512ac5
Manpreet Bahl <manpreetsingh.bahl@gmail.com>
2018-11-24 15:09:04 -0800

    Pushing prototype of minimax

--- minimax.py
+++ minimax.py
@@ -1,9 +1,54 @@
 from board import Board
 import numpy as np
+from random import shuffle
 
-def minimax_value(state):
-    if state.won:
-        return 9999
-    elif state.move == 1:
+def minimax(s):
+    currentPlayer = s.turn
+
+    def minimax_value(state):
+        #This game is a win
+        if state.won and state.turn == currentPlayer:
+            return 1
+
+        #This game is a lose
+        elif state.won and state.turn != currentPlayer:
+            return -1
+
+        #The game is a draw so return 0
+        elif state.full and state.won is False:
+            return 0
+
         successors = state.moves()
+        scores = []
+        for s in successors:
+            scores.append(minimax_value(s))
+        
+        if state.turn == currentPlayer:
+            return max(scores)
+        else:
+            return min(scores)
+    
+    successors = s.moves()
+    scores = []
+    for i in successors:
+        scores.append(minimax_value(i))
+
+    print(scores)
+
+    return successors[scores.index(max(scores))]
+    
+
+test = Board(size=3)
+print("Player: " + str(test.turn) + "'s Move")
+print("Board")
+print(test)
+
+while test.won is False:
+    test = minimax(test)
+    print("Player: " + str(test.turn) + "'s Move")
+    print("Board")
+    print(test)
+
+
+
         
