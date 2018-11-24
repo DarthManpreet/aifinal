607b7b96df49de4a841079f4556665f77a6355b5
Asher Toback <atoback@pdx.edu>
2018-11-23 21:08:24 -0800

    Finished 3x3 TTT RL Alg.
    A working RL algorithm which can train an agent to play tic-tac-toe of board size up to 3x3. The agent plays against itself during the training phase, making moves based on an epsilon-greedy approach. The Q-table is implemented as a 2d array, which is the reason the board can't be larger than 3x3, as the array gets intractably large otherwise. The reward scheme is as follows: receive a reward if the agent plays a winning move, and receive a negative reward if the agent plays a move such that the opponent can win next turn.  After training, the agent plays against itself, choosing moves based on the maximum Q-value in the Q-table. These games are printed out to the screen for viewing.
    
    The agent will play winning moves if available, and is smart enough to also stop all immediate threats his opponent poses. However, after 250,000 moves the agent didn't learn to stop all double attack moves. May need to adjust hyperparameters or let the algorithm train longer.

--- TicTacToe_RL.py
+++ TicTacToe_RL.py
@@ -1,38 +1,49 @@
 from board import Board
+from copy import deepcopy
 import numpy as np
 import random
+import json
 np.set_printoptions(threshold=np.nan)
 
 ETA = 1
 GAMMA = 0.9
+BOARDSIZE = 3
+ITERATIONS = 50000
+EPSILON = 10
 
-# Need a way to save qtable between runs. This would allow the AI
-# to retain its knowledge and potentially play against human or
-# other AI opponents. Possibly export to `.txt` file.
-#
-# May want to develop better reward scheme.
-#
-# Should play with ETA and GAMMA values
-#
-# Q-learning Neural Net if time permits. Would need a database of
-# 4x4 games to learn from. Best if they were `expert` games.
 
-# board_state = np.array([[0,0,0,0],
-#                         [1,2,1,1],
-#                         [1,2,1,1],
-#                         [1,2,1,1]])
+def json_save(table, size):
+    try:
+        if size == 2:
+            with open('qtable2x2.txt', 'w') as outfile:
+                json.dump(table.tolist(), outfile)
+        elif size == 3:
+            with open('qtable3x3.txt', 'w') as outfile:
+                json.dump(table.tolist(), outfile)
+        else:
+            with open('qtable4x4.txt', 'w') as outfile:
+                json.dump(table.tolist(), outfile)
+    except:
+        print "Save fail"
 
-# board_init = np.array([[1,2],
-#                        [2,2]])
-#
-# board0 = Board(2, None, 0)
-# board1 = board0.moves()[0]
-# board2 = board1.moves()[0]
-# board3 = board1.moves()[1]
-# board_restart = board0
-#
-# q_table = np.zeros((3**(board0.size * board0.size), 2*(board0.size * board0.size)))
-# print q_table.shape
+
+def json_load(size):
+    try:
+        if size == 2:
+            with open('qtable2x2.txt') as json_file:
+                table = json.load(json_file)
+        elif size == 3:
+            with open('qtable3x3.txt') as json_file:
+                table = json.load(json_file)
+        else:
+            with open('qtable4x4.txt') as json_file:
+                table = json.load(json_file)
+        print "found file"
+    except:
+        table = np.zeros((3**(size * size), 2*(size * size)))
+        print "File not found"
+    return table
+
 
 def update(qtable, state, next_state, next_turn):
     state_idx = 0
@@ -49,30 +60,45 @@ def update(qtable, state, next_state, next_turn):
     move_x = move_location[0][0]
     move_y = move_location[1][0]
     if action[move_x, move_y] == 2:
-        action_idx = move_x + size * move_y
+        action_idx = size * move_x + move_y
     else:
-        action_idx = move_x + size * move_y + size*size
+        action_idx = size * move_x + move_y + size*size
     # print state_idx, action_idx
     qtable_value = qtable[state_idx, action_idx]
     # print "current qtable value: ", qtable_value
     qtable_half = qtable.shape[1] / 2
-    if next_turn == 0:
+    if next_turn == 1:
         next_reward = best_next_action(qtable[:, qtable_half:], next_state)
     else:
         next_reward = best_next_action(qtable[:, :qtable_half], next_state)
-    qtable_update = qtable_value + ETA * (reward(next_state) + GAMMA * next_reward - qtable_value)
+    qtable_update = qtable_value + ETA * (reward(next_state, next_turn) + GAMMA * next_reward - qtable_value)
     qtable[state_idx, action_idx] = qtable_update
     return
 
 
-def reward(state):
-    temp_board = Board(len(state), state, None)
+def reward(state, turn):
+    temp_board = Board(len(state), state, turn)
+    # old_temp_board = Board(len(old_state), old_state, turn)
+    # print turn
     if temp_board.won:
-        # print temp_board
-        # print "\n\n\nFound winning move\n\n\n! Reward: ", 10.0
-        return 1.0
+        # print "I won!", temp_board
+        return 10.0
     else:
-        return 0.0
+        oppo_moves = temp_board.moves()
+        oppo_wins = False
+        for i in range(len(oppo_moves)):
+            oppo_moves[i].isWon()
+            if oppo_moves[i].won == True:
+                # print "Originally\n", old_temp_board, "I moved\n", temp_board, "but he won :(\n", oppo_moves[i]
+                oppo_wins = True
+        if oppo_wins:
+            return -10.0
+        else:
+            return 0.0
+
+# def two_unblocked():
+#     return
+
 
 def best_next_action(qtable, state):
     state_idx = 0
@@ -87,33 +113,93 @@ def best_next_action(qtable, state):
     # print "Max reward for next state: ", np.max(qtable[state_idx])
     return np.max(qtable[state_idx])
 
-# print board0
-# print board1
-# print board2
-# print board0.state - board1.state
-#
-# print q_table
-# update(q_table, board0.state, board1.state, board0.turn)
-# print q_table
-# update(q_table, board1.state, board2.state, board1.turn)
-# print q_table
-# update(q_table, board1.state, board3.state, board1.turn)
-# print q_table
-# update(q_table, board1.state, board2.state, board1.turn)
-# print q_table
-# update(q_table, board_restart.state, board1.state, board_restart.turn)
-# print q_table
+def choose_best_move(qtable, board, Playing):
+    state_idx = 0
+    size = board.state.shape[0]
+    for i in range(size):
+        for j in range(size):
+            if board.state[i][j] == 0:
+                state_idx += 1 * 3**(i + (size * j))
+            elif board.state[i][j] == 1:
+                state_idx += 2 * 3**(i + (size * j))
 
-board = Board(3, None, 1)
-q_table = np.zeros((3**(board.size * board.size), 2*(board.size * board.size)))
-for i in range(100000):
-    possible_moves = board.moves()
-    random_move = random.randint(0, len(possible_moves)-1)
-    next_board = possible_moves[random_move]
-    update(q_table, board.state, next_board.state, board.turn)
-    board = next_board
-    if board.full or board.won:
+    qtable_half = qtable.shape[1] / 2
+    if board.turn == 1:
+        move_options = qtable[state_idx, qtable_half:]
+    else:
+        move_options = qtable[state_idx, :qtable_half]
+
+    def move_index(x):
+        return x[1] + board.size * x[0]
+
+    available_squares = np.argwhere(board.state == 2)
+    available_squares = np.array(map(move_index, available_squares))
+    max_squares = np.where(move_options == np.max(move_options))[0]
+    best_squares = np.intersect1d(available_squares, max_squares)
+    if len(best_squares) != 0:
+        best_move = random.choice(best_squares)
+    else:
+        best_move = random.choice(available_squares)
+
+
+    move_x, move_y = divmod(best_move, board.size)
+    temp_board = deepcopy(board)
+    temp_board.state[move_x][move_y] = temp_board.turn
+
+    if temp_board.turn == 0:
+        temp_board.turn = 1
+    else:
+        temp_board.turn = 0
+
+
+    if Playing:
+        print "move options", move_options
+        print "available squares", available_squares
+        print "max squares", max_squares
+        print "best squares", best_squares
+        print "best move", best_move
+        print temp_board
+
+    return temp_board
+
+
+tttboard = Board(BOARDSIZE, None, 0)
+# q_table = load_q_table(board.size)
+q_table = np.array(json_load(tttboard.size))
+# print q_table
+for i in range(ITERATIONS):
+    if random.randint(0, 100) < EPSILON:
+    # if tttboard.turn == 0:
+        possible_moves = tttboard.moves()
+        random_move = random.randint(0, len(possible_moves)-1)
+        next_board = possible_moves[random_move]
+    else:
+        next_board = choose_best_move(q_table, tttboard, False)
+    update(q_table, tttboard.state, next_board.state, next_board.turn)
+    tttboard = next_board
+    tttboard.isWon()
+    if np.count_nonzero(tttboard.state == 2) == 0 or tttboard.won:
         # print "\n...restarting...\n"
-        board = Board(board.size, None, random.randint(0, 1))
+        tttboard = Board(tttboard.size, None, random.randint(0, 1))
 
-print "Minimum", np.min(q_table[np.nonzero(q_table)])
+print "Done Training"
+
+json_save(q_table, tttboard.size)
+
+print "Done saving"
+
+board_play = Board(tttboard.size, None, 1)
+num_games = 0
+while num_games < 10:
+    board_play = choose_best_move(q_table, board_play, True)
+    board_play.isWon()
+    if board_play.won:
+        print "won!"
+        num_games += 1
+        board_play = Board(BOARDSIZE, None, random.randint(0, 1))
+    if np.count_nonzero(board_play.state == 2) == 0:
+        print "board full"
+        num_games += 1
+        board_play = Board(BOARDSIZE, None, random.randint(0, 1))
+
+
