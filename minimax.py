72dd581017e02756f201bcaab322a390fcb2bf98
Manpreet Bahl <manpreetsingh.bahl@gmail.com>
2018-11-24 16:38:57 -0800

    Brute force minimax done

--- minimax.py
+++ minimax.py
@@ -3,41 +3,50 @@ import numpy as np
 from random import shuffle
 
 def minimax(s):
-    currentPlayer = s.turn
-
-    def minimax_value(state):
+    def minimax_value(state, maximize):
         #This game is a win
-        if state.won and state.turn == currentPlayer:
+        if state.won and maximize:
             return 1
 
         #This game is a lose
-        elif state.won and state.turn != currentPlayer:
+        elif state.won and not maximize:
             return -1
 
         #The game is a draw so return 0
         elif state.full and state.won is False:
             return 0
 
+        #Generate successors
         successors = state.moves()
-        scores = []
-        for s in successors:
-            scores.append(minimax_value(s))
-        
-        if state.turn == currentPlayer:
-            return max(scores)
+
+        #If MAX is to move
+        if maximize:
+            value = -9999
+            for s in successors:
+                value = max(value, minimax_value(s, False))
         else:
-            return min(scores)
+            value = 9999
+            for s in successors:
+                value = min(value, minimax_value(s, True))
+        
+        return value
     
+    #For all successors of the current state, evaluate their minimax values
     successors = s.moves()
     scores = []
     for i in successors:
-        scores.append(minimax_value(i))
+        scores.append(minimax_value(i, True))
 
-    print(scores)
-
-    return successors[scores.index(max(scores))]
+    #Convert it into numpy array for tie breaking scenarios
+    maxValue = max(scores)
+    scores = np.array(scores)
+    indices = np.argwhere(scores == maxValue).flatten()
+    
+    #Pick a random move if there's multiple moves with the same minimax value
+    return successors[np.random.choice(indices)]
     
 
+#For testing purposes
 test = Board(size=3)
 print("Player: " + str(test.turn) + "'s Move")
 print("Board")
