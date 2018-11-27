8057842821c866b8fa307a578bc085c358c89792
Manpreet Bahl <manpreetsingh.bahl@gmail.com>
2018-11-27 14:33:02 -0800

    Heuristics

--- minimax.py
+++ minimax.py
@@ -2,7 +2,7 @@ from board import Board
 import numpy as np
 from random import shuffle
 
-DEPTH=20000
+DEPTH=10
 
 """
 def minimax(s, depth):
@@ -57,126 +57,115 @@ def minimax(s, depth):
     return successors[np.random.choice(indices)]
 """
 
-def evaluate(s):
+def evaluate(s, player):
     score = 0
-    opponent = int(not s.turn)
+    opponent = int(not player)
 
     for i in range(s.size):
         #Current row has no opponent pieces
         if np.count_nonzero(s.state[i, :] == opponent) == 0:
             #Check row for our markers
-            if np.count_nonzero(s.state[i, :] == s.turn) == 1:
+            if np.count_nonzero(s.state[i, :] == player) == 1:
                 score += 1
-            elif np.count_nonzero(s.state[i, :] == s.turn) == 2:
+            elif np.count_nonzero(s.state[i, :] == player) == 2:
                 score += 50
-            elif np.count_nonzero(s.state[i, :] == s.turn) == 3:
-                score += 1000
+            elif np.count_nonzero(s.state[i, :] == player) == 3:
+                score = np.Inf
     
         #Current row has none of my pieces
-        if np.count_nonzero(s.state[i, :] == s.turn) == 0:
+        if np.count_nonzero(s.state[i, :] == player) == 0:
             #Check row for opponent markers
             if np.count_nonzero(s.state[i, :] == opponent) == 1:
                 score -= 1
             elif np.count_nonzero(s.state[i, :] == opponent) == 2:
                 score -= 50
             elif np.count_nonzero(s.state[i, :] == opponent) == 3:
-                score -= 1000
+                score = -np.inf
         
         #Current column has no opponent pieces
         if np.count_nonzero(s.state[:, i] == opponent) == 0:
             #Check column for our markers
-            if np.count_nonzero(s.state[:, i] == s.turn) == 1:
+            if np.count_nonzero(s.state[:, i] == player) == 1:
                 score += 1
-            elif np.count_nonzero(s.state[:, i] == s.turn) == 2:
+            elif np.count_nonzero(s.state[:, i] == player) == 2:
                 score += 50
-            elif np.count_nonzero(s.state[:, i] == s.turn) == 3:
-                score += 1000
+            elif np.count_nonzero(s.state[:, i] == player) == 3:
+                score = np.Inf
         
         #Current column has none of my pieces
-        if np.count_nonzero(s.state[:, i] == s.turn) == 0:
+        if np.count_nonzero(s.state[:, i] == player) == 0:
             #Check column for opponent markers
             if np.count_nonzero(s.state[:, i] == opponent) == 1:
                 score -= 1
             elif np.count_nonzero(s.state[:, i] == opponent) == 2:
                 score -= 50
             elif np.count_nonzero(s.state[:, i] == opponent) == 3:
-                score -= 1000
+                score = -np.inf
         
     #Check diagonals for our markers
     if np.count_nonzero(np.diag(s.state) == opponent) == 0:
-        if np.count_nonzero(np.diag(s.state) == s.turn) == 1:
+        if np.count_nonzero(np.diag(s.state) == player) == 1:
             score += 1
-        elif np.count_nonzero(np.diag(s.state) == s.turn) == 2:
+        elif np.count_nonzero(np.diag(s.state) == player) == 2:
             score += 50
-        elif np.count_nonzero(np.diag(s.state) == s.turn) == 3:
-            score += 1000
+        elif np.count_nonzero(np.diag(s.state) == player) == 3:
+            score = np.Inf
 
     #Check diagonals for opponent markers
-    if np.count_nonzero(np.diag(s.state) == s.turn) == 0:
+    if np.count_nonzero(np.diag(s.state) == player) == 0:
         if np.count_nonzero(np.diag(s.state) == opponent) == 1:
             score -= 1
         elif np.count_nonzero(np.diag(s.state) == opponent) == 2:
             score -= 50
         elif np.count_nonzero(np.diag(s.state) == opponent) == 3:
-            score -= 1000
+            score = -np.inf
     
     #Check the other diagonal
     if np.count_nonzero(np.diag(np.fliplr(s.state)) == opponent) == 0:
-        if np.count_nonzero(np.diag(np.fliplr(s.state)) == s.turn) == 1:
+        if np.count_nonzero(np.diag(np.fliplr(s.state)) == player) == 1:
             score += 1
-        elif np.count_nonzero(np.diag(np.fliplr(s.state)) == s.turn) == 2:
+        elif np.count_nonzero(np.diag(np.fliplr(s.state)) == player) == 2:
             score += 50
-        elif np.count_nonzero(np.diag(np.fliplr(s.state)) == s.turn) == 3:
-            score += 1000
+        elif np.count_nonzero(np.diag(np.fliplr(s.state)) == player) == 3:
+            score = np.Inf
 
-    if np.count_nonzero(np.diag(np.fliplr(s.state)) == s.turn) == 0:
+    if np.count_nonzero(np.diag(np.fliplr(s.state)) == player) == 0:
         if np.count_nonzero(np.diag(np.fliplr(s.state)) == opponent) == 1:
             score -= 1
         elif np.count_nonzero(np.diag(np.fliplr(s.state)) == opponent) == 2:
             score -= 50
         elif np.count_nonzero(np.diag(np.fliplr(s.state)) == opponent) == 3:
-            score -= 1000
+            score = -np.inf
 
     return score
 
 def minimax(s, depth):
+    player = s.turn
+
     def minimax_value(state, depth, alpha, beta, maximize):
-        if (depth == 0) or (state.won and maximize) or (state.won and not maximize) or (state.full and state.won is False):
-            return evaluate(state)
-
-        """
-        if state.won and maximize:
-            return 10000
-
-        #This game is a lose
-        elif state.won and not maximize:
-            return -10000
-
-        #The game is a draw so return 0
-        elif state.full and state.won is False:
-            return evaluate(state)
-        """
-
+        if (depth == 0) or (state.won) or (state.full):
+            return evaluate(state, player)
+            
         #Generate successors
         successors = state.moves()
 
         #If MAX is to move
         if maximize:
-            value = -np.inf
+            value = evaluate(state, player)
             for s in successors:
                 value = max(value, minimax_value(s, depth - 1, alpha, beta, False))
                 alpha = max(alpha, value)
-                if alpha >= beta:
+                if beta <= alpha:
                     break
-            return value
+            return alpha
         else:
-            value = np.inf
+            value = evaluate(state, player)
             for s in successors:
                 value = min(value, minimax_value(s, depth - 1, alpha, beta, True))
                 beta = min(beta, value)
-                if alpha >= beta:
+                if beta <= alpha:
                     break
-            return value
+            return beta
         #return value
     
     #For all successors of the current state, evaluate their minimax values
@@ -193,7 +182,8 @@ def minimax(s, depth):
     indices = np.argwhere(scores == maxValue).flatten()
     
     #Pick a random move if there's multiple moves with the same minimax value
-    return successors[np.random.choice(indices)] 
+    return successors[np.random.choice(indices)]
+
 
 #For testing purposes
 test = Board(size=3)
