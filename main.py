59635d832d3b94d9bc80aa274a05e88c706fa6ef
Manpreet Bahl <manpreetsingh.bahl@gmail.com>
2018-12-01 17:58:55 -0800

    new main

--- main.py
+++ main.py
@@ -2,6 +2,56 @@ from board import Board
 import minimax as Minimax
 import TicTacToe_RL as RL
 import random
+import matplotlib.pyplot as plt
+
+
+TRIALS = 5
+ROUNDS = 10
+
+q_table = RL.json_load(3)
+
+for t in range(TRIALS):
+  roundWins = []
+  print("=" * 30 + "Trial " + str(t+1) + "=" * 30)
+  
+  for r in range(ROUNDS):
+    who_first = random.random()
+
+    # 1 = Minimax, 0 = RL
+    if who_first > 0.5:
+      board = Board(size=3)
+    else:
+      board = Board(size=3, new_turn=0)
+
+    while True:
+      if board.turn == 1:
+        board = Minimax.minimax(board, 10)
+      else:
+        board = RL.choose_best_move(q_table, board, False)
+
+      if board.won:
+        if board.turn == 1:
+          #print("RL won!")
+          roundWins.append("R")
+        else:
+          #print("Minimax won!")
+          roundWins.append("M")
+        break
+      elif board.full and board.won is False:
+        #print("It's a tie!")
+        roundWins.append("T")
+        break
+  
+  print("Minimax Wins: " + str(roundWins.count("M")))
+  print("RL Wins: " + str(roundWins.count("R")))
+  print("Tied Games: " + str(roundWins.count("T")))
+  print("=" * 67)
+    
+"""
+from board import Board
+import minimax as Minimax
+import TicTacToe_RL as RL
+import random
 
 
 
@@ -41,4 +91,5 @@ while curr_board.won is False and not curr_board.full:
       elif curr_board.full:
         print("Its a tie!!!")
     else:
-      print("Its a tie!!")
+print("Its a tie!!")
+"""
--- minimax.py
+++ minimax.py
@@ -4,59 +4,6 @@ from random import shuffle
 
 DEPTH=10
 
-"""
-def minimax(s, depth):
-    def minimax_value(state, depth, maximize):
-        #This game is a win
-        #print("max value", maximize)
-        if state.won and maximize and depth == 0:
-            return 1
-
-        #This game is a lose
-        elif state.won and not maximize and depth == 0:
-            return -1
-
-        #The game is a draw so return 0
-        elif state.full and state.won is False and depth == 0:
-            return 0
-
-        #print("received state", state)
-        #Generate successors
-        successors = state.moves()
-        #print("Suucessors inside", successors)
-
-
-        #If MAX is to move
-        if maximize:
-            value = -9999
-            for s in successors:
-                value = max(value, minimax_value(s, depth-1, False))
-        else:
-            value = 9999
-            for s in successors:
-                value = min(value, minimax_value(s, depth-1, True))
-        #print("returning value", value)
-        return value
-    
-    #For all successors of the current state, evaluate their minimax values
-    successors = s.moves()
-    #print("Suucessors", successors)
-    scores = []
-    for i in successors:
-        #print("Now sending", i)
-        scores.append(minimax_value(i, depth, True))
-
-    #print("scores", scores)
-
-    #Convert it into numpy array for tie breaking scenarios
-    maxValue = max(scores)
-    scores = np.array(scores)
-    indices = np.argwhere(scores == maxValue).flatten()
-    
-    #Pick a random move if there's multiple moves with the same minimax value
-    return successors[np.random.choice(indices)]
-"""
-
 def evaluate(s, player):
     score = 0
     opponent = int(not player)
